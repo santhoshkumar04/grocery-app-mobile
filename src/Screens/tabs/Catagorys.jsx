89f4331be1866a0a04b3a3e8947f8ace7b535001
santhoshkumar04 <santhosh.kumar.p.0405@gmail.com>
2024-04-04 19:55:48 +0530

    changes commit

--- src/Screens/tabs/Catagorys.jsx
+++ src/Screens/tabs/Catagorys.jsx
@@ -1,10 +1,10 @@
 import {
   View,
   Text,
-  ScrollView,
   FlatList,
   Image,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -67,45 +67,62 @@ const Catagorys = () => {
           renderItem={renderSideItem}
         />
       </View>
-      <SafeAreaView style={{ flex: 1 }}>
-        <FlatList
-          numColumns="3"
-          data={vegetables}
-          style={{ paddingHorizontal: 14 }}
-          renderItem={({ item, i }) => (
-            <TouchableOpacity
-              style={{
-                alignItems: "center",
-                justifyContent: "space-around",
-                width: "32%",
-                padding: 6,
-                borderRadius: 12,
-                gap: 5,
-                marginBottom: 6,
-                marginRight: 6,
-                // opacity: 0.4,
-                // backgroundColor: "blue",
-              }}
-            >
-              <Image
-                source={{ uri: item.image }}
-                resizeMode="contain"
-                style={{
-                  width: "100%",
-                  height: 80,
-                  backgroundColor: "white",
-                  borderRadius: 12,
-                }}
+      <SafeAreaView style={{ flex: 1, paddingHorizontal: 14, paddingTop: 3 }}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {[...new Array(6)].map((item) => (
+            <View>
+              <View style={{ paddingVertical: 10 }}>
+                <Text style={{ fontSize: 18, fontWeight: 500 }}>
+                  Vegatables
+                </Text>
+              </View>
+              <FlatList
+                numColumns="3"
+                data={vegetables}
+                scrollEnabled={false}
+                style={{}}
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item, i }) => (
+                  <TouchableOpacity
+                    style={{
+                      alignItems: "center",
+                      justifyContent: "space-around",
+                      width: "32%",
+                      padding: 6,
+                      borderRadius: 12,
+                      gap: 5,
+                      marginBottom: 6,
+                      marginRight: 6,
+                      // opacity: 0.4,
+                      // backgroundColor: "blue",
+                    }}
+                  >
+                    <Image
+                      source={{ uri: item.image }}
+                      resizeMode="contain"
+                      style={{
+                        width: "100%",
+                        height: 80,
+                        backgroundColor: "white",
+                        borderRadius: 12,
+                      }}
+                    />
+                    <Text
+                      numberOfLines={2}
+                      style={{
+                        textAlign: "center",
+                        fontSize: 13,
+                        fontWeight: 500,
+                      }}
+                    >
+                      {item.name}
+                    </Text>
+                  </TouchableOpacity>
+                )}
               />
-              <Text
-                numberOfLines={2}
-                style={{ textAlign: "center", fontSize: 13, fontWeight: 500 }}
-              >
-                {item.name}
-              </Text>
-            </TouchableOpacity>
-          )}
-        />
+            </View>
+          ))}
+        </ScrollView>
       </SafeAreaView>
     </View>
   );
